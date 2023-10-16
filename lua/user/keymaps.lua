3cbe5164d2d8586ce1879f6d2e352f75a8920967
MuteSTEVE <Dr.MuteSTEVE2@protonmailcom>
2023-10-16 13:03:01 +0700

    Moving from after/ to lua/user/plugins

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -5,12 +5,16 @@ vim.g.mapleader = ' '
 local map = vim.api.nvim_set_keymap
 local mapset = vim.keymap.set
 
-map("n", "<leader>h", ":nohlsearch<cr>", {silent = true})
-map("n", "<leader>w", ":w<cr>", {})
-map("n", "<leader>s", ":so<cr>", {})
-map("n", "<leader>q", ":q<cr>", {})
-map("n", "<leader><S-q>", ":q!<cr>", {})
-map("n", "<leader><S-l>", ":Lazy<cr>", {})
+-- cmp-dictionary
+map("n", "<leader>en", "<cmd>set spelllang=en<CR>", {})
+map("n", "<leader>id", "<cmd>set spelllang=id<CR>", {})
+
+map("n", "<leader>h", "<cmd>nohlsearch<cr>", {silent = true})
+map("n", "<leader>w", "<cmd>w<cr>", {})
+map("n", "<leader>s", "<cmd>so<cr>", {})
+map("n", "<leader>q", "<cmd>q<cr>", {})
+map("n", "<C-q>", "<cmd>q!<cr>", {})
+map("n", "<S-l>", "<cmd>Lazy<cr>", {})
 
 -- Some useful keybinds VSCode move line keybind
 map('n', '<Left>', '<C-w>h', { noremap = true, silent = false })
@@ -18,15 +22,15 @@ map('n', '<Down>', '<C-w>j', { noremap = true, silent = true })
 map('n', '<Up>', '<C-w>k', { noremap = true, silent = true })
 map('n', '<Right>', '<C-w>l', { noremap = true, silent = true })
 
-map('n', 'K', ":move .-2<CR>==", { noremap = true, silent = true })
-map('n', 'J', ":move .+1<CR>==", { noremap = true, silent = true })
-map('v', 'K', ":move '<-2<CR>gv-gv", { noremap = true, silent = true })
-map('v', 'J', ":move '>+1<CR>gv-gv", { noremap = true, silent = true })
+map('n', 'K', "<cmd>move .-2<CR>==", { noremap = true, silent = true })
+map('n', 'J', "<cmd>move .+1<CR>==", { noremap = true, silent = true })
+map('v', 'K', "<cmd>move '<-2<CR>gv-gv", { noremap = true, silent = true })
+map('v', 'J', "<cmd>move '>+1<CR>gv-gv", { noremap = true, silent = true })
 
 -- buffer movement
-map('n', 'Z', ':bp<CR>', { noremap = true, silent = true})
-map('n', 'C', ':bn<CR>', { noremap = true, silent = true})
-map('n', 'X', ':bd<CR>', { noremap = true, silent = true})
+map('n', 'Z', '<cmd>bp<CR>', { noremap = true, silent = true})
+map('n', 'C', '<cmd>bn<CR>', { noremap = true, silent = true})
+map('n', 'X', '<cmd>bd<CR>', { noremap = true, silent = true})
 
 -- Move between panes quickly
 map('n', '<C-h>', '<C-w>h', {noremap = true, silent = false})
@@ -37,8 +41,6 @@ map('n', '<C-k>', '<C-w>k', {noremap = true, silent = false})
 -- Get out of insert mode quickly
 map('i', 'ka', '<ESC>', {noremap = true, silent = false})
 map('i', 'ak', '<ESC>', {noremap = true, silent = false})
-map('v', 'ak', '<ESC>', {noremap = true, silent = false})
-map('v', 'ka', '<ESC>', {noremap = true, silent = false})
 
 -- Indent
 map('v', '<', '<gv', {noremap = true, silent = false})
