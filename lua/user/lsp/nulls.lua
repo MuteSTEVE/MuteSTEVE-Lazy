e9a5f63c85bafe6e4cf2f2889298043b542471ec
MuteSTEVE <Dr.MuteSTEVE2@protonmailcom>
2023-11-19 13:27:06 +0700

    Adding python to null-ls

--- lua/user/lsp/nulls.lua
+++ lua/user/lsp/nulls.lua
@@ -3,17 +3,42 @@ if not status_ok then
   return
 end
 
+local diagnostics = null_ls.builtins.diagnostics
 local formatting = null_ls.builtins.formatting
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 null_ls.setup({
   sources = {
-    formatting.prettier, formatting.black, formatting.gofmt, formatting.shfmt,
-    formatting.clang_format, formatting.cmake_format, formatting.dart_format,
+    formatting.prettier,
+    formatting.black,
+    formatting.gofmt,
+    formatting.mypy,
+    formatting.ruff,
     formatting.lua_format.with({
       extra_args = {
         '--no-keep-simple-function-one-line', '--no-break-after-operator', '--column-limit=100',
         '--break-after-table-lb', '--indent-width=2'
       }
-    }), formatting.isort, formatting.codespell.with({filetypes = {'markdown'}})
+    }),
+    formatting.isort,
+    formatting.codespell.with({filetypes = {'markdown'}}),
+
+    diagnostics.mypy,
+    diagnostics.ruff,
   },
+  on_attach = function(client, bufnr)
+    if client.supports_method["textDocument/formatting"] then
+      vim.api.nvim_clear_autocmds({
+        group = augroup,
+        buffer = bufnr,
+      })
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        group = augroup,
+        buffer = bufnr,
+        callback = function ()
+          vim.lsp.buf.format({ bufnr = bufnr })
+        end
+      })
+    end
+  end,
 })
--- lua/user/plugins/ufo.lua
+++ lua/user/plugins/ufo.lua
@@ -1,12 +1,12 @@
-vim.o.foldcolumn = '1' -- '0' is not bad
-vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+-- vim.o.foldcolumn = '1' -- '0' is not bad
+-- vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
 vim.o.foldlevelstart = 99
 vim.o.foldenable = true
 
-vim.keymap.set('n', 'zo', "<cmd>foldopen<cr>")
-vim.keymap.set('n', 'zO', require('ufo').openAllFolds)
-vim.keymap.set('n', 'zc', "<cmd>foldclose<cr>")
-vim.keymap.set('n', 'zC', require('ufo').closeAllFolds)
+vim.keymap.set('n', '=', "<cmd>foldopen<cr>")
+vim.keymap.set('n', '+', require('ufo').openAllFolds)
+vim.keymap.set('n', '-', "<cmd>foldclose<cr>")
+vim.keymap.set('n', '_', require('ufo').closeAllFolds)
 vim.keymap.set('n', 'zK', function ()
   local winid = require("ufo").peekFoldedLinesUnderCursor()
   if not winid then
@@ -19,3 +19,11 @@ require('ufo').setup({
     return { 'lsp', 'indent' }
   end
 })
+
+vim.cmd([[
+augroup remember_folds
+autocmd!
+autocmd BufWinLeave *.* mkview
+autocmd BufWinEnter *.* silent! loadview
+augroup END
+]])
